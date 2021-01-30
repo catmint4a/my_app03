57fc952be91f63d1da1f24dbce1a44158c2225cb
catmint4a <catmint_4a@hotmail.com>
2021-01-30 20:42:20 +0900

    テストの追加

--- src/test/controllers/static_pages_controller_test.rb
+++ src/test/controllers/static_pages_controller_test.rb
@@ -4,4 +4,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "should get home" do
+    get root_path
+    assert_response :success
+    # assert_select "title", "Ruby on Rails Tutorial Sample App"
+  end
 end
