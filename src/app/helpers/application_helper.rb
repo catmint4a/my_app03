0b7ae483a2be8f072dd6fbd7c6f64cd875010dd8
catmint4a <catmint_4a@hotmail.com>
2021-01-31 19:33:58 +0900

    test helper on title

--- src/app/helpers/application_helper.rb
+++ src/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Return Full title
+  def full_title(page_title = '')
+    base_title = 'ぽすといっと！'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
