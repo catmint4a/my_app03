1f4e084cdce2fe94a25d5e6ab4cb1b567b3a3748
catmint4a <catmint_4a@hotmail.com>
2021-01-31 18:41:13 +0900

    add static pages test

--- src/test/controllers/static_pages_controller_test.rb
+++ src/test/controllers/static_pages_controller_test.rb
@@ -10,4 +10,14 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     # assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
+  test "should get about" do
+    get about_path
+    assert_response :success
+    # assert_select "title", "Ruby on Rails Tutorial Sample App"
+  end
+  test "should get help" do
+    get help_path
+    assert_response :success
+    # assert_select "title", "Ruby on Rails Tutorial Sample App"
+  end
 end
