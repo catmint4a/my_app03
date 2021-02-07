677c3bb69545a8fd4535c8e6a8fbea47facbf827
catmint4a <catmint_4a@hotmail.com>
2021-02-07 23:00:07 +0900

    add static page links

--- src/spec/requests/static_pages_request_spec.rb
+++ src/spec/requests/static_pages_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "StaticPages", type: :request do
   describe "#home" do
     it "responds successfully" do
-      get '/'
+      get root_path
       expect(response).to be_successful
     end
   end
@@ -11,6 +11,24 @@ RSpec.describe "StaticPages", type: :request do
     it "responds successfully" do
       get help_path
       expect(response).to be_successful
+    end
+  end
+  describe "#about" do
+    it "responds successfully" do
+      get about_path
+      expect(response).to be_successful
+    end
+  end
+
+  describe "link in home" do
+    before do
+      visit root_path
+    end
+    it "help link" do
+      expect(page).to have_link 'ヘルプ'
+    end
+    it "about link" do
+      expect(page).to have_link 'このサイトについて'
     end
   end
 end
