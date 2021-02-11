3fc3696aa00e08411d0f1ce9ddeb0f50cea074d7
catmint4a <catmint_4a@hotmail.com>
2021-02-11 19:02:56 +0900

    user login logout integration test

--- src/spec/factories/users.rb
+++ src/spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
+    # name { "MyString" }
+    # email { "MyString" }
+    name { "Aaron" }
+    email { "tester@example.com" }
+    password { "password" }
   end
 end
