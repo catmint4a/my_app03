91f27512d7ba674597f026cc1c060210c16f5de6
catmint4a <catmint_4a@hotmail.com>
2021-02-11 21:07:49 +0900

    show user name in home

--- src/app/helpers/application_helper.rb
+++ src/app/helpers/application_helper.rb
@@ -1,8 +1,5 @@
 module ApplicationHelper
-  # before_action :show_current_user
-  # def show_current_user
-  #   @user = current_user
-  # end
+
   # Return Full title
   def full_title(page_title = '')
     base_title = 'ぽすといっと！'
@@ -12,4 +9,5 @@ module ApplicationHelper
       page_title + ' | ' + base_title
     end
   end
+
 end
