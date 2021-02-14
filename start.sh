679d89b84806662d5f919e9f6b477ac4f0d891c5
catmint4a <catmint_4a@hotmail.com>
2021-02-14 23:53:31 +0900

    production

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 if [ "${RAILS_ENV}" = "production" ]; then
     yarn install
-    bundle exec rails assets:precompile
+    bundle exec rails assets:precompile RAILS_ENV=production
 fi
 
 bundle exec rails s -p ${PORT:-3000} -b 0.0.0.0
