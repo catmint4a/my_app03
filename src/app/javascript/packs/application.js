e9ad5d30c5113c552da9e87f5c9a3b7814dfcf85
t <catmint_4a@hotmail.com>
2021-02-14 13:16:12 +0900

    application.js changed

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -7,9 +7,23 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
 require("jquery")
 import "bootstrap"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
