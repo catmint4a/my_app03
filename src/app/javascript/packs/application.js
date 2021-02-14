3258d3526273c7badfb489cd20562df4f84fc76e
catmint4a <catmint_4a@hotmail.com>
2021-02-14 19:48:44 +0900

    //= require jquery

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -10,6 +10,10 @@ require("channels")
 require("jquery")
 import "bootstrap"
 
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
