7c4648567985480c4e90de77ce72d09f63eee183
catmint4a <catmint_4a@hotmail.com>
2021-02-16 03:28:09 +0900

    config.yml

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ "${RAILS_ENV}" = "production" ]; then
-    yarn install
     bundle exec rails assets:precompile RAILS_ENV=production
 fi
-
+yarn install
+bundle exec bin/webpack
 bundle exec rails s -p ${PORT:-3000} -b 0.0.0.0
