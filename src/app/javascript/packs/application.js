b43043708e19ee8324167674c768432ddd594357
t <catmint_4a@hotmail.com>
2021-02-17 08:16:10 +0900

    appjs

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -3,12 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "jquery"
 require("jquery")
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
 import "bootstrap"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
 
 //= require jquery
 //= require_tree .
