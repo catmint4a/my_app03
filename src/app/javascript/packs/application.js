f053111bee0e41444941546ee1fe78f4517b8bc7
t <catmint_4a@hotmail.com>
2021-02-16 12:46:13 +0900

    appjs fix

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -3,19 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("jquery")
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
 import "bootstrap"
-import Rails from 'rails-ujs'
-Rails.start()
-//= require jquery
-//= require jquery_ujs
-//= require rails-ujs
-//= require turbolinks
-//= require_tree 
+
+
+//= require_tree .
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
