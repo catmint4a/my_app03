730a2ff9c431459e4273de7100bf1ab6965434ba
t <catmint_4a@hotmail.com>
2021-02-17 21:20:26 +0900

    assets ok

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ if [ "${RAILS_ENV}" = "production" ]; then
     bundle exec rails assets:precompile
 fi
 
-bundle exec rails s -p ${PORT:-3000} -b 0.0.0.0
+bundle exec rails s -p ${PORT:-3000} -b 0.0.0.0ß
