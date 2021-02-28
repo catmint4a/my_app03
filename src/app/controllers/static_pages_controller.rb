d97b6e4085018a04e264165fbd23ee9fa7cc284c
catmint4a <catmint_4a@hotmail.com>
2021-02-28 23:12:19 +0900

    micropost feed

--- src/app/controllers/static_pages_controller.rb
+++ src/app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
 
   def home
     @micropost = current_user.microposts.build if logged_in?
+    @feed_items = current_user.feed.paginate(page: params[:page])# if current_user.feed
   end
   
   def help
