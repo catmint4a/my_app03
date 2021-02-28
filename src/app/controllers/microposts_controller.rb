6c4673a26570311998a8599466dbef917d3b5ba1
catmint4a <catmint_4a@hotmail.com>
2021-02-28 23:52:56 +0900

    micropost feed blank instance

--- src/app/controllers/microposts_controller.rb
+++ src/app/controllers/microposts_controller.rb
@@ -8,7 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "投稿しました"
       redirect_to root_url
     else
-      @feed_item = current_user.feed.paginate(page: params[:page])
+      @feed_items = current_user.feed.paginate(page: params[:page])
       render 'static_pages/home'
     end
   end
--- src/app/controllers/static_pages_controller.rb
+++ src/app/controllers/static_pages_controller.rb
@@ -1,8 +1,10 @@
 class StaticPagesController < ApplicationController
 
   def home
-    @micropost = current_user.microposts.build if logged_in?
-    @feed_items = current_user.feed.paginate(page: params[:page])# if current_user.feed
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
   
   def help
