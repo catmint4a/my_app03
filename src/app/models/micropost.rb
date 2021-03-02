b42d339cf4fb3a2fbbca821a1fa94dd1b1f9540c
t <catmint_4a@hotmail.com>
2021-03-02 22:47:48 +0900

    image post

--- src/app/models/micropost.rb
+++ src/app/models/micropost.rb
@@ -4,4 +4,12 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validates :image , content_type: { in: %W[image/jpeg image/gif image/png],
+                                    message: "有効な画像ファイルを指定してください" },
+                            size: { less_than: 3.megabytes,
+                                    message: "画像は3MB以下のものを指定してください"}
+
+  def display_image
+    image.variant(resize_to_limit: [500, 500])
+  end
 end
