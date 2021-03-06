18cacc9a3ac4c7ad4009f210bce059171b3c286e
t <catmint_4a@hotmail.com>
2021-03-06 23:17:27 +0900

    relationsip model fix indent

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
-                                  has_many :passive_relationships, class_name: "Relationship",
+  has_many :passive_relationships, class_name: "Relationship",
                                   foreign_key: "followed_id",
                                   dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
