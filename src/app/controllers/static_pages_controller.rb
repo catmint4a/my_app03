ab3aab5adb2007e44b50dd816678ac65ea41d3a7
catmint4a <catmint_4a@hotmail.com>
2021-03-14 09:50:04 +0900

    guest login

--- src/app/controllers/static_pages_controller.rb
+++ src/app/controllers/static_pages_controller.rb
@@ -12,4 +12,14 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def new_guest
+    user = User.find_or_create_by!(name: 'guestuser', email: 'guest@example.com') do |user|
+      user.password = SecureRandom.alphanumeric
+      user.password_confirmation = user.password
+    end
+    log_in user
+    flash[:success] = 'ゲストユーザーとしてログインしました。'
+    redirect_to root_path
+  end
 end
