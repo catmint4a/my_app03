df436226944771e68a795a2184a14a2412a84170
t <catmint_4a@hotmail.com>
2021-03-17 17:40:54 +0900

    like syste

--- src/app/controllers/microposts_controller.rb
+++ src/app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
     before_action :correct_user, only: :destroy
+    before_action :like_feed, only: :like
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -20,6 +21,10 @@ class MicropostsController < ApplicationController
     redirect_to request.referrer || root_url
   end
 
+  def like
+    @like_posts = like_feed.paginate(page: params[:page])
+  end
+
   private
 
     def micropost_params
@@ -29,5 +34,10 @@ class MicropostsController < ApplicationController
     def correct_user
       @micropost = current_user.microposts.find_by(id: params[:id])
       redirect_to root_url if @micropost.nil?
+    end
+
+    def like_feed
+      like_ids = "SELECT micropost_id FROM likes WHERE user_id = #{current_user.id}"
+      Micropost.where("id IN (#{like_ids})")
     end
 end
