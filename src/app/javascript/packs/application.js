aa71a7e6de115bb3fa7bbaf24c51540f07f3e1b4
t <catmint_4a@hotmail.com>
2021-03-24 22:21:26 +0900

    gem version up

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
 import "bootstrap"
 //= require_tree .
 
