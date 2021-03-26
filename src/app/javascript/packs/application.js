cf045a9c884438abf5cd5b0a001bd3fe5b5e50c1
t <catmint_4a@hotmail.com>
2021-03-26 23:41:32 +0900

    delete jquery

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -7,9 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
+// require("jquery")
 import "bootstrap"
-//= require_tree .
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
