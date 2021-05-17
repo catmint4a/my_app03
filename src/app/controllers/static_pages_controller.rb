4d02260a2941769e4216bc133be59d67fd6fb5f0
catmint4a <catmint_4a@hotmail.com>
2021-05-17 21:34:06 +0900

    deploy service

--- src/app/controllers/static_pages_controller.rb
+++ src/app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
 
   def home
+    head 200
     if logged_in?
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page]).search(params[:search])
