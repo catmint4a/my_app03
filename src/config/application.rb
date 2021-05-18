146df3f07fbf85670db71c361a3f69a5d5fc656a
t <catmint_4a@hotmail.com>
2021-05-18 21:17:35 +0900

    setting host in application.rb

--- src/config/application.rb
+++ src/config/application.rb
@@ -31,5 +31,6 @@ module App
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.hosts << IPAddr.new('192.168.0.0/16')
   end
 end
